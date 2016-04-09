0eeb0d7abdae2d99544da0d7a526c99a9fc00084
sunyan-sky <sunyan-sky@126.com>
2016-04-09 20:48:47 +0800

    create project ,frist commit

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/abc', function(req, res, next) {
+  res.json({name:'abc'});
+});
+
+
+module.exports = router;
